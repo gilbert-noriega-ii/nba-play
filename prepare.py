9d091e33dbde9fe70b705247c19b74a1f7fa98d6
gilbert-noriega-ii <gilbert.noriega.ii@gmail.com>
2020-10-28 20:52:49 -0500

    adding scaling function

--- prepare.py
+++ prepare.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 from sklearn.model_selection import train_test_split
-
+import sklearn.preprocessing
 
 
 
@@ -60,21 +60,6 @@ def conference_split(nba):
     return nba
 
 
-def season_split(nba):
-    '''
-    This function splits the data into different nba seasons
-    '''
-    #nba 14-15 season
-    nba14_15 = nba.loc[:2459,:]
-    #nba 15-16 season
-    nba15_16 = nba.loc[2460:4919,:]
-    #nba 16-17 season
-    nba16_17 = nba.loc[4920:7379,:]
-    #nba 17-18 season
-    nba17_18 = nba.loc[7380:,:]
-    
-    return nba14_15, nba15_16, nba16_17, nba17_18
-
 def nba_split(df):
     '''
     This function splits a dataframe into train, validate, and test sets
@@ -83,58 +68,54 @@ def nba_split(df):
     train, validate = train_test_split(train_and_validate, train_size = .7, random_state=123, stratify=train_and_validate.W)
     return train, validate, test
 
-def wrangle_nba_14_15():
+def wrangle_nba():
     '''
-    This function splits the nba14_15 season into train, validate, and test sets
+    This function splits the nba seasons into train, validate, and test sets
     '''
     #save csv as a dataframe
     nba = pd.read_csv('nba.games.stats.csv')
     #preppring dataframe nba stats
     nba = prep_nba(nba)
-    #splitting into seasons
-    nba14_15, nba15_16, nba16_17, nba17_18 = season_split(nba)
     #splitting into train, validate test
-    train, validate, test = nba_split(nba14_15)
+    train, validate, test = nba_split(nba)
     return train, validate, test
 
-def wrangle_nba_15_16():
+def scaled_wrangle_nba():
     '''
-    This function splits the nba15_16 season into train, validate, and test sets
-    '''
-    #save csv as a dataframe
-    nba = pd.read_csv('nba.games.stats.csv')
-    #preppring dataframe nba stats
-    nba = prep_nba(nba)
-    #splitting into seasons
-    nba14_15, nba15_16, nba16_17, nba17_18 = season_split(nba)
-    #splitting into train, validate test
-    train, validate, test = nba_split(nba15_16)
-    return train, validate, test
-
-def wrangle_nba_16_17():
-    '''
-    This function splits the nba16_17 season into train, validate, and test sets
+    This function scales the data and splits 
+    the nba seasons into train, validate, and test sets
     '''
     #save csv as a dataframe
     nba = pd.read_csv('nba.games.stats.csv')
     #preppring dataframe nba stats
     nba = prep_nba(nba)
-    #splitting into seasons
-    nba14_15, nba15_16, nba16_17, nba17_18 = season_split(nba)
     #splitting into train, validate test
-    train, validate, test = nba_split(nba16_17)
-    return train, validate, test
-
-def wrangle_nba_17_18():
-    '''
-    This function splits the nba17_18 season into train, validate, and test sets
-    '''
-    #save csv as a dataframe
-    nba = pd.read_csv('nba.games.stats.csv')
-    #preppring dataframe nba stats
-    nba = prep_nba(nba)
-    #splitting into seasons
-    nba14_15, nba15_16, nba16_17, nba17_18 = season_split(nba)
-    #splitting into train, validate test
-    train, validate, test = nba_split(nba17_18)
+    train, validate, test = nba_split(nba)
+    #assigns the scaling method as min-max scaler
+    scaler = sklearn.preprocessing.MinMaxScaler()
+    #identifies the columns to scale
+    columns_to_scale = ['FieldGoals.', 'X3PointShots.', 'FreeThrows.', 'OffRebounds', 'TotalRebounds', 'Assists', 'Steals', 'Blocks', 'Turnovers', 'TotalFouls', 'Opp.FieldGoals.', 'Opp.3PointShots.', 'Opp.FreeThrows.', 'Opp.OffRebounds', 'Opp.TotalRebounds', 'Opp.Assists', 'Opp.Steals', 'Opp.Blocks', 'Opp.Turnovers', 'Opp.TotalFouls']
+    #adds '_scaled' to the end of the newly scaled columns to identify differences
+    new_column_names = [c + '_scaled' for c in columns_to_scale]
+    #fts the columns to the scaler
+    scaler.fit(train[columns_to_scale])
+    #concatonates the newly created scaled columns to their respective data sets,
+    #adds 'new_column_names' as the label to the added columns
+    #uses the original index since the new columns no longer have an index
+    train = pd.concat([
+        train,
+        pd.DataFrame(scaler.transform(train[columns_to_scale]), columns=new_column_names, index=train.index),
+    ], axis=1)
+    validate = pd.concat([
+        validate,
+        pd.DataFrame(scaler.transform(validate[columns_to_scale]), columns=new_column_names, index=validate.index),
+    ], axis=1)
+    test = pd.concat([
+        test,
+        pd.DataFrame(scaler.transform(test[columns_to_scale]), columns=new_column_names, index=test.index),
+    ], axis=1)
+    #drops non-scaled data
+    train = train.drop(columns = {'FieldGoals.', 'X3PointShots.', 'FreeThrows.', 'OffRebounds', 'TotalRebounds', 'Assists', 'Steals', 'Blocks', 'Turnovers', 'TotalFouls', 'Opp.FieldGoals.', 'Opp.3PointShots.', 'Opp.FreeThrows.', 'Opp.OffRebounds', 'Opp.TotalRebounds', 'Opp.Assists', 'Opp.Steals', 'Opp.Blocks', 'Opp.Turnovers', 'Opp.TotalFouls'})
+    validate = validate.drop(columns = {'FieldGoals.', 'X3PointShots.', 'FreeThrows.', 'OffRebounds', 'TotalRebounds', 'Assists', 'Steals', 'Blocks', 'Turnovers', 'TotalFouls', 'Opp.FieldGoals.', 'Opp.3PointShots.', 'Opp.FreeThrows.', 'Opp.OffRebounds', 'Opp.TotalRebounds', 'Opp.Assists', 'Opp.Steals', 'Opp.Blocks', 'Opp.Turnovers', 'Opp.TotalFouls'})
+    test = test.drop(columns = {'FieldGoals.', 'X3PointShots.', 'FreeThrows.', 'OffRebounds', 'TotalRebounds', 'Assists', 'Steals', 'Blocks', 'Turnovers', 'TotalFouls', 'Opp.FieldGoals.', 'Opp.3PointShots.', 'Opp.FreeThrows.', 'Opp.OffRebounds', 'Opp.TotalRebounds', 'Opp.Assists', 'Opp.Steals', 'Opp.Blocks', 'Opp.Turnovers', 'Opp.TotalFouls'})
     return train, validate, test
